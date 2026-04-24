f623cf7e744a806d66743a91f1102683f8081d5e
Maël <maelandre2@gmail.com>
2026-04-24 13:02:46 +0200

    implement client-side message encryption and decryption

--- clients/crypto.py
+++ clients/crypto.py
@@ -1,8 +1,12 @@
 import secrets
+import os.path
+import json
 import base64
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes 
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.exceptions import InvalidTag
 
 
 def generate_session_key():
@@ -33,4 +37,61 @@ def encrypt_session_key(session_key, public_key_pem):
 
     return encrypted_key
 
+def generate_nonce():
+    nonce = secrets.token_bytes(12)
 
+    return nonce
+
+def encrypt_message(message, session_key):
+    data = message.encode("utf-8")
+
+    nonce = generate_nonce()
+
+    cipher = Cipher(
+        algorithms.AES(session_key),
+        modes.GCM(nonce)
+    )
+
+    encryptor = cipher.encryptor()
+
+    ciphertext = encryptor.update(data) + encryptor.finalize()
+
+    tag = encryptor.tag
+
+    payload_dict = {
+        "nonce_b64" : base64.b64encode(nonce).decode("utf-8"),
+        "ciphertext_b64" : base64.b64encode(ciphertext).decode("utf-8"),
+        "tag_b64": base64.b64encode(tag).decode("utf-8")
+    }
+
+    payload_json = json.dumps(payload_dict)
+    return payload_json
+
+def decrypt_message(payload_json, session_key):
+    payload_dict = json.loads(payload_json)
+
+    nonce = base64.b64decode(payload_dict["nonce_b64"])
+    ciphertext = base64.b64decode(payload_dict["ciphertext_b64"])
+    tag = base64.b64decode(payload_dict["tag_b64"])
+
+    cipher = Cipher(
+        algorithms.AES(session_key),
+        modes.GCM(nonce, tag)
+    )
+
+    decryptor = cipher.decryptor()
+
+    try:
+        decrypted_message = decryptor.update(ciphertext) + decryptor.finalize()
+    except InvalidTag:
+        print("Tag invalide.")
+
+    return decrypted_message.decode("utf-8")
+
+
+if __name__ == "__main__":
+    session_key = generate_session_key()
+    payload_json = encrypt_message("bonjour", session_key)
+    print(payload_json)
+
+    print(decrypt_message(payload_json, session_key))
