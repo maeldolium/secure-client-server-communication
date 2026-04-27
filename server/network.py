5efa2c934221b63883cd279ebb24af16397c92c2
Maël <maelandre2@gmail.com>
2026-04-27 15:19:07 +0200

    add server creation and send PEM public key to client

---
+++ server/network.py
@@ -0,0 +1,24 @@
+import socket, json
+
+HOST, PORT = "localhost", 9999
+
+def network(public_key):
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(1)
+
+    while True:
+        client, address = server.accept()
+
+        public_key_dict = {
+            "type" : "public_key",
+            "data" : public_key.decode()
+        }
+
+        public_key_json = json.dumps(public_key_dict)
+
+        client.sendall(public_key_json.encode())
+
+        client.close()
+        server.close()
